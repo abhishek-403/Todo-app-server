64e8e8d3607994376b5b46e8aa27d7a25a6e532b
Abhishek Sharma <abhishek79009@gmail.com>
2023-05-31 19:09:33 +0530

    check 2

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cookieParser())
 app.use(express.json({ limit: '10mb' }))
 app.use(cors({
     credentials: true,
-    origin:"ttps://yournewnotes.netlify.app"
+    origin:"https://yournewnotes.netlify.app"
 }))
 
 
