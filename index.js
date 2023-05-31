fa76cdbe48dbb9fc8f7c8d2003f3409ea0ca9697
Abhishek Sharma <abhishek79009@gmail.com>
2023-05-31 19:48:03 +0530

    dnner

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const app = express();
 
 const cors = require('cors')
 let origin = "http://localhost:3000"
-if(process.env.NODE_ENV==='production'){
-    origin=process.env.CLIENT_URL
+if (process.env.NODE_ENV.trim() === "production") {
+    origin = process.env.CLIENT_URL
 }
-
 app.use(cookieParser())
 app.use(express.json({ limit: '10mb' }))
 app.use(cors({
